0bf871471f38f5f625937dbc45283f1a156520b3
prodipdev <prodipink@gmail.com>
2023-05-03 16:35:46 +0600

    Send chef data api.

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const port = process.env.PORT || 5000;
+
+const chef = require("./data/chefList.json");
+const recipes = require("./data/recipes.json");
+app.use(cors());
+
+// send all chef
+app.get("/chef", (req, res) => {
+  res.send(chef);
+});
 
 app.listen(port, () => {
   console.log(`server is running on port: ${port}`);
